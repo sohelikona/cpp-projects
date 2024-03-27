8c9dd7d02e8642aef2ffc35f660a4a398c57bbed
sohelikona <kritiakter0@gmail.com>
2024-03-27 23:09:35 +0600

    Update encryption.cpp

--- msg-encription/encryption.cpp
+++ msg-encription/encryption.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+bool performCeaserCipher(string& content, bool encrypt) {
+    int shift = encrypt ? 3 : -3;
+
+    for (char& ch: content) {
+        if (isalpha(ch)) {
+            char base = isupper(ch) ? 'A' : 'a';
+            ch = static_cast<char>((ch - base + shift + 26) % 26 + base);
+        }
+    }
+
+    return true;
+}
+
 bool encryptfile(const string& filename, bool encrypt) {
     ifstream inFile(filename);
     if (!inFile) {
@@ -12,4 +25,11 @@ bool encryptfile(const string& filename, bool encrypt) {
 
     string content((istreambuf_iterator<char>(inFile)), {});
     inFile.close();
+    if (performCeaserCipher(content, encrypt)) {
+        ofstream outFile(encrypt ? "encrypted_" + filename : "decrypted_" + filename);
+        if (!outFile) {
+            return false;
+        }
+        outFile << content;
+    }
 }
